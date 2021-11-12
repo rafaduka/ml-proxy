8b653493fd7bba00f69b51c7720fe285e03cacb6
Rafael Horacio <rholiveira@uolinc.com>
2021-11-12 01:37:05 -0300

    refactor: refactor variables names and updated order fields

--- api-monitor/src/main/java/com/mercadolivre/control/MonitorApplication.java
+++ api-monitor/src/main/java/com/mercadolivre/control/MonitorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ControlApplication {
+public class MonitorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ControlApplication.class, args);
+		SpringApplication.run(MonitorApplication.class, args);
 	}
 
 }
