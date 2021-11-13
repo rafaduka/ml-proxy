8a54e833302d7716db8f1611acb64e398a1c4046
Rafael Horacio <rholiveira@uolinc.com>
2021-11-12 22:49:21 -0300

    feat: logs added to show info at console

--- api-monitor/src/main/java/com/mercadolivre/control/controller/StatisticController.java
+++ api-monitor/src/main/java/com/mercadolivre/control/controller/StatisticController.java
@@ -4,6 +4,8 @@ import com.mercadolivre.control.dto.StatisticDTO;
 import com.mercadolivre.control.entity.Statistic;
 import com.mercadolivre.control.service.StatisticService;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +19,7 @@ import java.util.stream.Collectors;
 @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:4200"})
 public class StatisticController {
 
+    Logger logger = LoggerFactory.getLogger(StatisticController.class);
     private StatisticService statisticService;
     private ModelMapper modelMapper;
 
@@ -27,7 +30,7 @@ public class StatisticController {
 
     @GetMapping
     public ResponseEntity<List<StatisticDTO>> findAll() {
-
+        logger.info("find all Statistics from database");
         final List<Statistic> statistics = statisticService.findAll();
 
         final List<StatisticDTO> response = statistics.stream()
@@ -41,13 +44,14 @@ public class StatisticController {
     public ResponseEntity<StatisticDTO> create(@RequestBody @Valid StatisticDTO statisticDTO) {
         Statistic statistic = statisticService.create(modelMapper.map(statisticDTO, Statistic.class));
         StatisticDTO response = modelMapper.map(statistic, StatisticDTO.class);
-
+        logger.info("Statistics were created total = {}", statisticDTO.getTotal());
         return new ResponseEntity(response, HttpStatus.CREATED);
     }
 
     @DeleteMapping
     public ResponseEntity<StatisticDTO> deleteAll() {
         statisticService.deleteAll();
+        logger.warn("mongodb was dropped");
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
